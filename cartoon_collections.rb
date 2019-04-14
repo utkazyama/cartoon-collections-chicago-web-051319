23311ac4d8a879a86857e1c4ddfe5cb744c6b2ed
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-14 05:48:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array,item)# code an argument here
   # Your code here
   array.each_with_index do |item,i|
+  puts "i"
 end
 
 def summon_captain_planet# code an argument here
