e9dc94e1d895e0b56f25e75c7ed5ba47f57fa1c8
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-14 05:49:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 array=["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def roll_call_dwarves(array,item)# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |item,i|
-  puts "i"
+  puts "#{i}. #{item}"
 end
 
 def summon_captain_planet# code an argument here
