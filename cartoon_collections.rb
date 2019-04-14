b290576b22487d6db06c9250c144d0a7dba45651
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-14 06:04:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map.capitalize{}
+  planeteer_calls.map.capitalize{|i|}
 end
 
 def long_planeteer_calls# code an argument here
