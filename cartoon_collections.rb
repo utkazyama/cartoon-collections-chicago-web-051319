266f99b5dca16cc4bfe6751da2b9117f41525130
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-14 06:38:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese_types.include?
+    cheese_types.include?(cheese)
   
 end
